31d033f8b043fee0557b2e2e46aecf73e68cfdf7
Aksi <aksaule124026@gmail.com>
2022-01-18 11:49:56 +0600

    Create new model words

--- project/english_words/models.py
+++ project/english_words/models.py
@@ -1,3 +1,19 @@
 from django.db import models
-
 # Create your models here.
+
+
+class Words(models.Model):
+    part_speech = [
+        ('noun', 'Noun'),
+        ('verb', 'Verb'),
+        ('adjective', 'Adjective'),
+        ('pronoun', 'Pronoun'),
+        ('adverb', 'Аdverb'),
+        ('numeral', 'Numeral'),
+    ]
+
+    word = models.CharField(verbose_name="Word", max_length=100)
+    part_of_word = models.CharField(verbose_name="Part_speech", max_length=100, choices=part_speech)
+
+    def __str__(self):
+        return self.word + " " + self.part_of_word
